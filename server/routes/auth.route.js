57ad2a8c696a39a07b007cbdd9db96177d17f22d
Diego Pucci <me@diegopucci.com>
2017-10-13 17:47:47 +0200

    moved env vars in package - removed required JWT_SECRET

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -13,7 +13,9 @@ router.route('/login')
 
 /** GET /api/auth/random-number - Protected route,
  * needs token returned by the above as header. Authorization: Bearer {token} */
+/*
 router.route('/random-number')
   .get(expressJwt({ secret: config.jwtSecret }), authCtrl.getRandomNumber);
+  */
 
 export default router;
