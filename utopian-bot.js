f96c9575755303e10597378c381d61a1d9991733
Diego Pucci <me@diegopucci.com>
2017-11-13 12:11:10 +0000

    fix starting bot

--- utopian-bot.js
+++ utopian-bot.js
@@ -37,7 +37,7 @@ conn.once('open', function ()
         const secondsago = (new Date().getTime() - new Date(botStatus.last_vote_time + "Z").getTime()) / 1000;
         const votingPower = botStatus.voting_power + (10000 * secondsago / 432000);
 
-        if (votingPower <= limitPower && !forced) {
+        if (votingPower < limitPower && !forced) {
           console.log("UPS I AM SO TIRED TODAY. VOTED TOO MUCH", votingPower);
           conn.close();
           process.exit(0);
