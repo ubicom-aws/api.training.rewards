70d5fe2dc0c6201bd57bafb4c8c643eb29c589f5
samrg472 <samrg472@gmail.com>
2017-12-19 11:41:45 -0800

    Use the universal post updater in the updater-posts script

--- src/updater-posts.ts
+++ src/updater-posts.ts
@@ -1,12 +1,11 @@
 import * as mongoose from 'mongoose';
-import * as Promise from 'bluebird';
 
+import { getUpdatedPost } from './server/controllers/post.controller/update';
 import Post from './server/models/post.model';
 import Stats from './server/models/stats.model';
-import config from './config/config';
 import steemApi from './server/steemAPI';
+import config from './config/config';
 
-(mongoose as any).Promise = Promise;
 mongoose.connect(config.mongo);
 
 const conn = mongoose.connection;
@@ -40,33 +39,10 @@ conn.once('open', function ()
           .then(posts => {
             if(posts.length > 0) {
               posts.forEach((post, index) => {
-                setTimeout(function(){
-                  steemApi.getContent(post.author, post.permlink, (err, updatedPost) => {
-                    if (!err) {
-                      console.log(`---- NOW CHECKING POST ${post.permlink} by ${post.author} ----\n`);
-
-                      updatedPost.json_metadata = JSON.parse(updatedPost.json_metadata);
-
-                      // @UTOPIAN @TODO bad patches. Needs to have a specific place where the put the utopian data so it does not get overwritten
-                      if (!updatedPost.json_metadata.type && post.json_metadata.type) {
-                        updatedPost.json_metadata.type = post.json_metadata.type;
-                      }
-                      if (updatedPost.json_metadata.app !== 'utopian/1.0.0') updatedPost.json_metadata.app = 'utopian/1.0.0';
-                      if (updatedPost.json_metadata.community !== 'utopian') updatedPost.json_metadata.community = 'utopian';
-                      // making sure the repository does not get deleted
-                      if (!updatedPost.json_metadata.repository) updatedPost.json_metadata.repository = post.json_metadata.repository;
-                      if (!updatedPost.json_metadata.platform) updatedPost.json_metadata.platform = post.json_metadata.platform;
-                      if (!updatedPost.json_metadata.pullRequests && post.json_metadata.pullRequests) updatedPost.json_metadata.pullRequests = post.json_metadata.pullRequests;
-
-                      updatedPost.json_metadata.type = updatedPost.json_metadata.type.replace("announcement-", "task-");
-
-                      for (var prop in updatedPost) {
-                        if (updatedPost[prop] !== post[prop]) {
-                          post[prop] = updatedPost[prop];
-                          console.log(`UPDATED PROP ${prop} was ${JSON.stringify(post[prop])} now is ${JSON.stringify(updatedPost[prop])}\n`);
-                        }
-                      }
-
+                setTimeout(async function() {
+                  try {
+                    const updatedPost = await getUpdatedPost(post.author, post.permlink);
+                    console.log(`---- NOW CHECKING POST ${post.permlink} by ${post.author} ----\n`);
                       post.save()
                         .then(() => console.log(`POST UPDATED SUCCESSFULLY\n`))
                         .catch(e => {
@@ -77,19 +53,18 @@ conn.once('open', function ()
                             conn.close();
                             process.exit(0);
                           }
-                        })
-                    } else {
-                      console.log(`CANNOT RETRIEVE POST - STEEM ERROR ${err}\n`);
-                      if ((index + 1) === count) {
-                        stats.stats_last_updated_posts = now;
-
-                        stats.save().then(savedStats => {
-                          conn.close();
-                          process.exit(0);
                         });
-                      }
+                  } catch (err) {
+                    console.log(`CANNOT RETRIEVE POST - STEEM ERROR ${err}\n`);
+                    if ((index + 1) === count) {
+                      stats.stats_last_updated_posts = now;
+
+                      stats.save().then(savedStats => {
+                        conn.close();
+                        process.exit(0);
+                      });
                     }
-                  });
+                  }
                 }, index * 3000);
               });
             }
