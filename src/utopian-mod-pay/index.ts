8dd99261978052a851d9e91a4afd7a00901832aa
samrg472 <samrg472@gmail.com>
2018-01-26 15:39:15 -0800

    Remove moderator account from comment permlinks - This can potentially cause invalid permlinks

--- src/utopian-mod-pay/index.ts
+++ src/utopian-mod-pay/index.ts
@@ -300,7 +300,7 @@ of the total amount of posts were accepted by moderators.
           await broadcast(mod, account, {
             parentAuthor: author,
             parentPermlink: permlink,
-            permlink: permlink + '-' + mod.moderator.account,
+            permlink: permlink,
             title
           });
         } catch (e) {
