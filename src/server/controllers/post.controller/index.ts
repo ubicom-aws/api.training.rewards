8d238c004f9b80eb43ab5f6b64c973a9333e8ec3
East <eastmaels@gmail.com>
2018-02-28 10:02:44 +0800

    add support for moderator's tags inline edit

--- src/server/controllers/post.controller/index.ts
+++ src/server/controllers/post.controller/index.ts
@@ -78,7 +78,7 @@ async function update(req, res, next) {
   const reviewed = getBoolean(req.body.reviewed);
   const contribType = req.body.type || null;
   const repo = req.body.repository || null;
-
+  const tags = req.body.tags || null;
   const questions = req.body.questions || [];
   const score = req.body.score ? parseFloat(req.body.score) : 0;
 
@@ -104,6 +104,9 @@ async function update(req, res, next) {
       }
       if (repo) {
         post.json_metadata.repository = repo;
+      }
+      if (tags) {
+        post.json_metadata.tags = tags;
       }
 
       if (reviewed) {
@@ -183,6 +186,7 @@ async function update(req, res, next) {
       post.markModified('json_metadata.repository');
       post.markModified('json_metadata.moderator');
       post.markModified('json_metadata.type');
+      post.markModified('json_metadata.tags');
       const savedPost = await post.save();
       sendPost(res, savedPost);
     } catch (e) {
