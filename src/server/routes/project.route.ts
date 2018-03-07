8181fba90f123fc2e55f11eacc80c2fc721f6c27
Nico Wehmöller <nico.wehmoeller@t-online.de>
2018-03-07 18:30:09 +0100

    Require Auth for projects creation/update/delete routes

--- src/server/routes/project.route.ts
+++ src/server/routes/project.route.ts
@@ -2,14 +2,15 @@ import * as express from 'express';
 import * as validate from 'express-validation';
 import paramValidation from '../../config/param-validation';
 import projectCtrl from '../controllers/project.controller';
+import {requireAuth} from "./middleware";
 
 const router = express.Router();
 
 router.route('/')
     .get(projectCtrl.list)
-    .post(validate(paramValidation.project.create), projectCtrl.create)
-    .put(validate(paramValidation.project.update), projectCtrl.update)
-    .delete(validate(paramValidation.project.remove), projectCtrl.remove)
+    .post(requireAuth, validate(paramValidation.project.create), projectCtrl.create)
+    .put(requireAuth, validate(paramValidation.project.update), projectCtrl.update)
+    .delete(requireAuth, validate(paramValidation.project.remove), projectCtrl.remove)
 ;
 
 
